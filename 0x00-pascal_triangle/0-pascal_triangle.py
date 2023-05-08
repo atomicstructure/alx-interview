0d60b3816b41eda9ae67c56cdf82bdbdfb4d637a
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2023-05-08 10:38:17 +0100

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Pascal's Triangle """
 
-def  pascal_triangle(n):
+
+def pascal_triangle(n):
     """ Returns a list of lists of integers representing 
         the Pascal's triangle
     """
